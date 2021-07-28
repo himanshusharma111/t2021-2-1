493624dfefe847571468d3230a748b7cd465955f
himanshusharma111 <56394562+himanshusharma111@users.noreply.github.com>
2021-07-28 21:39:19 +0530

    Update Problem2.java

--- First_Screening_t2021_2_1/Problem2.java
+++ First_Screening_t2021_2_1/Problem2.java
@@ -1,36 +1,36 @@
-	/*Problem-2: With a single integer as the input, generate the following until a = x [series of numbers as shown in below examples]
-
-Output: (examples)
-1) input a = 1, then output : 1
-2) input a = 2, then output : 1, 3
-3) input a = 3, then output : 1, 3, 5
-4) input a = 4, then output : 1, 3, 5, 7
-.
-.
-5) input a = x, then output : 1, 3, 5, 7, ......
-	 * */
 package First_Screening_t2021_2_1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Problem2 {
-	
+
 	public static void main(String[] args) {
+		int i = 0;
+		int j = 0;
+		int a; 
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the value :");
-		int a = sc.nextInt();
-
-		for(int i = 1; i<=a; i++)
-		{
-			// if the number is not divisible by 2. 
-			if (i % 2 != 0 ) {
-				a++;
-				System.out.print(i+",");
+		a = sc.nextInt();
+		
+		List<String> list = new ArrayList<String>();
+		
+		while (j < a) {
+			
+			// if the number is not divisible by 2.
+			
+			if (i % 2 != 0) {
+				j++;
+				list.add(String.valueOf(i));
 			}
+			
+			i++;
 		}
 		
-	
+		String s = String.join(",", list);
+		System.out.println(s);
 	}
 
 }
